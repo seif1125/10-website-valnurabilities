5c90087dbad7f455559349ebe7285b06ed027c5d
seif1125 <seifammar1125@gmail.com>
2023-06-16 17:01:46 +0300

    implement IDOR

--- index.php
+++ index.php
@@ -1,22 +1,20 @@
-
-//Cross-Site Scripting (XSS) attacks are a form of injection attack, 
-//where malicious scripts are injected into trusted web applications. 
-//A simple XSS attack will occur on a vulnerable website that accepts user input via a GET parameter and displays
-// the data on the website.
-//suppose we have a search page and user have field to search this search appear in our html https://example.com/search/?param
-//Injecting the following code into the URL enables an XSS attack
-//https://example.com/search/?param=window.location=”https://maliciouswebsite.com”
-// The injected code will cause a redirect to maliciouswebsite.com as soon as the site loads.
-// our solution for Preventing XSS in PHP
-
-   // Input Sanitization in PHP
-    //htmlentities() vs htmlspecialchars()
-    //strip_tags()
-    //addslashes()
-    
-// as we htmlentities() wil decode the html tags and strip_tags() will prevent injecting strip tag
+//An IDOR PHP vulnerability occurs when an application exposes internal object references directly in the URL 
+//or parameters. Attackers can manipulate these references to access unauthorized resources or perform actions 
+//they should not have permissions for.
+<?php
+$id = $_GET['id']; // User-supplied input for fetching an object
+$query = "SELECT * FROM products WHERE id = $id"; // Insecure direct object reference
+$result = mysqli_query($connection, $query);
+$product = mysqli_fetch_assoc($result);
+// Display product details
+?>
 
 
+// How to fix it?
+
+//Avoid exposing internal object references directly in URLs or parameters.
+//Implement proper access controls and permissions checks to prevent unauthorized access to sensitive resources.
+//Use indirect object references or enforce access control on the server-side.
 <html lang = "en">
    
    <head>
@@ -30,7 +28,5 @@
    </head>
 	
    <body>
-      
-   <h1>No results were found for: <?php echo htmlentities(addslashes(strip_tags($_GET['params']))); ?></h1>
    </body>
 </html>
